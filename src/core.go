69a7a7677833e6400b7bf85a0847b764d4d7c639
James Daniel <james.j.daniel@outlook.com>
2017-01-15 10:20:15 -0600

    Classed-based method addition

--- src/core.go
+++ src/core.go
@@ -1,16 +1,29 @@
-package main
+package hephaestus
 
 import (
   "fmt"
-  "flag"
-  "errors"
   "strings"
   "bytes"
 )
 
-// Parses the human's plaintext input.
-// It also validates whether plaintext is valid.
-func parseStdIn(humArgPtr *string)(int, error) {
+// Core contains state of the program.
+type Core struct {
+  humArgPtr *string
+}
+
+// NewCore instantiates a core object.
+func NewCore() *Core {
+  core := new(Core)
+  // core.humArgPtr = flag.String("i", "null",
+  //   "Accepts human definition of f/w rule.")
+  // flag.Parse()
+  // core.validateHuInt(core.humArgPtr)
+  // fmt.Println(core.rulesetStringBuilder(core.humArgPtr))
+  return core
+}
+
+// validateHuInt validates human-provided firewall rule
+func (self *Core) validateHuInt(humArgPtr *string)(int) {
   splitArr := strings.Split(*humArgPtr, " ")
 
   acceptedVerbs := map[string]bool {
@@ -34,30 +47,30 @@ func parseStdIn(humArgPtr *string)(int, error) {
     fmt.Println("[DEBUG] verb validity check: PASSED")
   } else {
     fmt.Println("[DEBUG] verb validity check: FAILED")
-    return -1, errors.New("statement contains invalid verb")
+    return -1
   }
 
   if acceptedFlow[strings.ToLower(splitArr[1])] {
     fmt.Println("[DEBUG] flow validity check: PASSED")
   } else {
     fmt.Println("[DEBUG] flow validity check: FAILED")
-    return -1, errors.New("statement contains invalid flow")
+    return -1
   }
 
   if acceptedProto[strings.ToLower(splitArr[2])] {
     fmt.Println("[DEBUG] protocol validity check: PASSED")
   } else {
     fmt.Println("[DEBUG] protocol validity check: FAILED")
-    return -1, errors.New("statement contains invalid protocol")
+    return -1
   }
 
   fmt.Println("[DEBUG] isvalidstatement: PASSED\n-------------------------------------")
-  return 0, nil
+  return 0
 }
 
 // Build the actual iptables-compatible ruleset.
 // At this point, the human input should have been validated.
-func rulesetStringBuilder(humArgPtr *string)(string){
+func (self *Core) rulesetStringBuilder(humArgPtr *string)(string){
   splitArr := strings.Split(*humArgPtr, " ")
   var buf bytes.Buffer
 
@@ -90,13 +103,4 @@ func rulesetStringBuilder(humArgPtr *string)(string){
       strings.ToUpper(splitArr[0])))
   }
   return buf.String()
-}
-
-func main() {
-  // Defines options for the human
-  var humArgPtr = flag.String("i", "null", "Accepts human definition of f/w rule.")
-  flag.Parse()
-
-  parseStdIn(humArgPtr)
-  fmt.Println(rulesetStringBuilder(humArgPtr))
 }
